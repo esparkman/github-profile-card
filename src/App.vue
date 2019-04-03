54ca741788aa32a48a3b339e26ef82128a3345d2
Evan Sparkman <me@evansparkman.com>
2019-04-02 22:07:11 -0400

    Add workspace settings, eslint config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="ui container">
+    <h1>Listeneers of 2019</h1>
+    <div class="ui cards">
+      <GithubUserProfile v-for="(username, index) in usernames" :username="username" :key="index"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import GithubUserProfile from "@/components/GithubUserProfile";
 export default {
-  name: "app",
+  name: "App",
   components: {
-    HelloWorld
+    GithubUserProfile
+  },
+  data() {
+    return {
+      usernames: [
+        "ssickles",
+        "esparkman",
+        "ealdent",
+        "iangawronski",
+        "wmavis",
+        "huntermeyer",
+        "david-frederick",
+        "claycribbs",
+        "dborchert",
+        "jmscholen"
+      ]
+    };
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/GithubUserProfile.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="ui card">
+    <div class="image">
+      <img :src="user.avatar_url" />
+    </div>
+    <div class="content">
+      <a :href="`https://github.com/${username}`" class="header">{{
+        user.name || "Name Less"
+      }}</a>
+      <div class="meta">
+        <span class="date"
+          >Joined Github in {{ user.created_at | joinYear }}</span
+        >
+      </div>
+      <div class="description">{{ user.bio }}</div>
+    </div>
+    <div class="extra content">
+      <a :href="`https://github.com/${username}/followers`">
+        <i class="user icon"></i>
+        {{ user.followers }} Followers
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import moment from "moment";
+export default {
+  name: "GithubUserProfile",
+  filters: {
+    joinYear(date) {
+      return moment(date).format("YYYY");
+    }
+  },
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      user: {}
+    };
+  },
+  created() {
+    axios
+      .get(`https://api.github.com/users/${this.username}`)
+      .then(response => {
+        this.user = response.data;
+      });
+  }
+};
+</script>
